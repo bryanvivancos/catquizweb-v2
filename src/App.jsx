86a1e72e6a25ace6307abc611d23d260bdee5883
bryan <bryanvivanco97@outlook.com>
2025-03-09 16:29:42 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Banner from './components/banner'
+
+function App() {
+  return (
+    <Banner></Banner>
+  )
+}
+
+export default App
---
+++ src/components/banner.jsx
@@ -0,0 +1,12 @@
+
+const Banner = () => {
+    return (
+        <div>
+            <img className="principalLogo" src= "src\assets\CATQUIZ_Logo_Blanco.png" alt="logo CatQuiz" />
+            <p>PROXIMAMENTE</p>
+        </div>
+        
+    )
+}
+
+export default Banner
